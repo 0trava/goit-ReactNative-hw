ee01ca5d8d1a61fa17bb7b587a165cde369e397b
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-06-13 00:16:40 +0300

    add PostsScreen

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -2,11 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import LoginScreen from './assets/Screens/LoginScreen';
 import RegistrationScreen from './assets/Screens/RegistrationScreen';
+import PostsScreen from './assets/Main/PostsScreen';
 
 export default function App() {
   return (
     // <LoginScreen />
-    <RegistrationScreen />
+    // <RegistrationScreen />
+    <PostsScreen />
+
 
 
 
---
+++ AwesomeProject/assets/Main/PostsScreen.js
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+export default function PostsScreen() {
+  return (
+    <View>
+        {/* HEADER */}
+        <View >
+        <Text>PostsScreen</Text>
+        </View>
+        {/* BODY */}
+        <View>
+        <Text>PostsScreen</Text>
+        </View>
+        {/* FOOTER */}
+        <View>
+        <Text>PostsScreen</Text>
+        </View>
+    </View>
+    
+  )
+}
+
+const styles = StyleSheet.create({})
