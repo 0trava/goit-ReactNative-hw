676f14f68a423622b0f35c0e5c8d770e916ee866
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-06-13 13:12:15 +0300

    add header PostsScreen

--- AwesomeProject/assets/Main/PostsScreen.js
+++ AwesomeProject/assets/Main/PostsScreen.js
@@ -1,19 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image,TouchableOpacity } from 'react-native';
 import React from 'react';
+
+import IconOut from '../images/postsscreen_icon_out.png';
 
 export default function PostsScreen() {
   return (
-    <View>
+    <View style={styles.container}>
         {/* HEADER */}
-        <View >
-        <Text>HEADER</Text>
+        <View style={styles.header} >
+        <Text style={styles.header_title}>Публікації</Text>
+        
+        <TouchableOpacity style={styles.header_icon} onPress={()=> {}}>
+            <Image source={IconOut} style={styles.header_iconOut} ></Image>
+        </TouchableOpacity>
+
         </View>
         {/* BODY */}
-        <View>
+        <View style={styles.body} >
         <Text>BODY</Text>
         </View>
         {/* FOOTER */}
-        <View>
+        <View style={styles.footer}>
         <Text>FOOTER</Text>
         </View>
     </View>
@@ -21,4 +28,64 @@ export default function PostsScreen() {
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+
+
+    },
+
+    header: {
+        position: "relative",
+        flex: 1,
+        justifyContent: "flex-end",
+        alignItems: "center",
+        paddingBottom: 11,
+        backgroundColor: "#FFFFFF",
+        // shadowColor: "rgba(0, 0, 0, 0.3)",
+        // shadowRadius: 0.5,
+        // elevation: 1,
+
+        shadowColor: 'black',
+        shadowOpacity: 0.3,
+        shadowOffset: { width: 0, height: 0.5},
+        shadowRadius: 0.5,
+        elevation: 3,
+    },
+
+    header_icon: {
+        position: 'absolute',
+        right: 10,
+        bottom: 10,
+
+
+
+    },
+
+    header_iconOut: {
+        width: 24,
+        height: 24,
+    },
+
+
+    body: {
+        flex: 7,
+        // backgroundColor: "#FF6C00",
+    },
+
+    footer: {
+        flex: 1,
+
+    },
+
+
+    header_title: {
+        fontFamily: 'Roboto',
+        fontWeight: 600,
+        fontSize: 17,
+        // lineHeight: 22,
+
+
+    },
+
+})
