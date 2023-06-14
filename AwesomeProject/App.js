efeb1778e6cc9aedc4439468dd77f631fc8e2556
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-06-14 12:19:47 +0300

    Finish HW 2

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,14 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import LoginScreen from './assets/Screens/LoginScreen';
-import RegistrationScreen from './assets/Screens/RegistrationScreen';
-import PostsScreen from './assets/Main/PostsScreen';
+import LoginScreen from './Components/LoginScreen';
+import RegistrationScreen from './Components/RegistrationScreen';
+import PostsScreen from './Screens/PostsScreen';
 
 export default function App() {
   return (
     // <LoginScreen />
-    // <RegistrationScreen />
-    <PostsScreen />
+    <RegistrationScreen />
+    // <PostsScreen />
 
 
 
